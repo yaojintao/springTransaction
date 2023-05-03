4a518988481bcf3877e7c3f55983b872bfadd729
cc <857460894@qq.com>
2023-05-03 21:35:49 +0800

    1

---
+++ src/main/java/com/PayServiceImpl.java
@@ -0,0 +1,21 @@
+package com;
+
+import jdk.jfr.events.ThrowablesEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class PayService {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Transactional
+    public void pay() {
+        String sql = "update user set name = '小名' where id =1";
+        jdbcTemplate.execute(sql);
+        int i = 1/0;
+    }
+}
